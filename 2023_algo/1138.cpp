3762893e2eaebc53fc994929d16e3a796b20b5c6
KoYoungin <cleanyoung1108@gmail.com>
2023-07-12 23:01:55 +0900

    20230712 한 줄로 서기

---
+++ 2023_algo/1138.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cin >> n;
+
+    vector<int> num(n);
+    vector<int> order(n, 11);
+
+    for (int i = 0; i < n; i++){
+        int h;
+        cin >> h;
+        num[i] = h;
+    }
+    for (int i = 1; i <= n; i++){
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if(cnt >= num[i-1] && order[j] == 11){
+                order[j] = i;
+                break;
+            }
+            if(order[j] > i)
+                cnt++;
+            
+        }
+    }
+    for (int i = 0; i < n; i++){
+        cout << order[i]<<" ";
+    }
+
+        return 0;
+}
